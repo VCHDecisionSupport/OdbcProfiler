2eef11e1b62ace562712b21ca50432a69677a482
Graham Crowell <graham.crowell@gmail.com>
2017-03-22 21:11:13 -0700

    use pyodbc.SQL_... to get connection info

--- pyGenericProfiler/AutoTestOdbc.py
+++ pyGenericProfiler/AutoTestOdbc.py
@@ -27,7 +27,6 @@ def columns_parser(columns_meta_row):
 """takes pyodbc.tables (ie. ODBC meta function SQLTables) record and returns dict of meta data"""
 def tables_parser(tables_meta_row):
     row_dict = {}
-    # row_dict['database_name'] = tables_meta_row[0]
     row_dict['schema_name'] = tables_meta_row[1]
     row_dict['table_name'] = tables_meta_row[2]
     row_dict['table_type'] = tables_meta_row[3]
@@ -35,6 +34,7 @@ def tables_parser(tables_meta_row):
     return row_dict
 
 class AutoTestOdbc(object):
+    """base class ODBC data source to be profiled"""
     def __init__(self, connection_lambda, server_name, database_name, server_type, ansi_column_table_format, ansi_column_format):
         self.connection_lambda = connection_lambda
         self.server_name = server_name
@@ -70,7 +70,10 @@ class AutoTestOdbc(object):
             try:
                 self.connection = pyodbc.connect(self.server_odbc_connection_string)
                 self.odbc_version = self.connection.getinfo(pyodbc.SQL_DRIVER_ODBC_VER)
-                print('\t\tconnected (ODBC version: {})'.format(self.odbc_version))
+                self.odbc_dbms_name = self.connection.getinfo(pyodbc.SQL_DBMS_NAME)
+                self.odbc_database_name = self.connection.getinfo(pyodbc.SQL_DATABASE_NAME)
+                self.odbc_server_name = self.connection.getinfo(pyodbc.SQL_SERVER_NAME)
+                print('\t\tconnected (ODBC version: {}; dbms_name: {}; database_name {}\nodbc_server_name: {})'.format(self.odbc_version, self.odbc_dbms_name, self.odbc_database_name, self.odbc_server_name))
             except Exception as e:
                 print(e)
                 raise e
@@ -86,14 +89,15 @@ class AutoTestOdbc(object):
             self.connection.close()
 
     def tables(self, **kwargs):
-        """returns pyodbc.cursor of all tables (see https://github.com/mkleehammer/pyodbc/wiki/Cursor#tablestablenone-catalognone-schemanone-tabletypenone)"""
+        """equivalent to SQLTables in ODBC C-API
+        (see https://github.com/mkleehammer/pyodbc/wiki/Cursor#tablestablenone-catalognone-schemanone-tabletypenone)"""
         self.connect()
         cur = self.connection.cursor()
         return cur.tables(**kwargs)
 
     
     def columns(self,**kwargs):
-        """returns pyodbc.cursor of all columns in current database
+        """equivalent to SQLColumns in ODBC C-API
         (see https://github.com/mkleehammer/pyodbc/wiki/Cursor#columnstablenone-catalognone-schemanone-columnnone)
         """
         self.connect()
@@ -102,17 +106,20 @@ class AutoTestOdbc(object):
 
     
     def process_meta_data(self):
+        """update meta data logs"""
         self.table_infos = {}
         print('process tables()')
         valid_schemas = set()
         for table_meta_row in self.tables():
             table_info = tables_parser(table_meta_row)
+            """skip sys and INFORMATION_SCHEMA"""
             if table_info['schema_name'] not in skip_schemas:
                 ansi_full_table_name = table_info['ansi_full_table_name']
                 self.table_infos[ansi_full_table_name] = tables_parser(table_meta_row)
                 valid_schemas.add(table_info['schema_name'])
         print('process columns()')
         self.column_infos = {}
+        """get columns in tables with non-skipped valid schemas"""
         for schema_name in valid_schemas:
             for column_meta_row in self.columns(schema=schema_name):
                 ansi_full_column_name = columns_parser(column_meta_row)['ansi_full_column_name']
@@ -121,6 +128,7 @@ class AutoTestOdbc(object):
         self.table_info_id_dict = {}
         for table_meta_dict in self.table_infos.values():
             table_meta_dict['database_info_id'] = self.database_info_id
+            """save to logging database with sqlalchemy"""
             table_info_id = self.profile_saver.log_table_info(**table_meta_dict)
             table_meta_dict['table_info_id'] = table_info_id
             self.table_info_id_dict[table_meta_dict['ansi_full_table_name']] = table_info_id
@@ -129,10 +137,12 @@ class AutoTestOdbc(object):
             if column_meta_dict['ansi_full_table_name'] in self.table_info_id_dict:
                 table_info_id = self.table_info_id_dict[column_meta_dict['ansi_full_table_name']]
             column_meta_dict['table_info_id'] = table_info_id
+            """save to logging database with sqlalchemy"""
             column_info_id = self.profile_saver.log_column_info(**column_meta_dict)
             column_meta_dict['column_info_id'] = column_info_id
     
     def profile_database(self):
+        """execute and log profiling queries"""
         profile_datetime = datetime.datetime.today()
         print('profile_database')
         print('\tprofile tables')
@@ -155,6 +165,7 @@ class AutoTestOdbc(object):
             table_profile['table_row_count_execution_seconds'] = table_row_count_execution_seconds
             table_row_count_datetime = profile_datetime
             table_profile['table_row_count_datetime'] = table_row_count_datetime
+            """save to logging database with sqlalchemy"""
             table_profile_id = self.profile_saver.log_table_profile(**table_profile)
             table_profile['table_profile_id'] = table_profile_id
             self.table_profiles[table_info_dict['ansi_full_table_name']] = table_profile
@@ -180,12 +191,12 @@ class AutoTestOdbc(object):
                 column_profile['column_distinct_count_execution_seconds'] = column_distinct_count_execution_seconds
                 column_distinct_count_datetime = profile_datetime
                 column_profile['column_distinct_count_datetime'] = column_distinct_count_datetime
+                """save to logging database with sqlalchemy"""
                 column_profile_id = self.profile_saver.log_column_profile(**column_profile)
                 column_profile['column_profile_id'] = column_profile_id
                 self.column_profiles[column_info_dict['ansi_full_column_name']] = column_profile
-
+                """group by counts to get frequencies of column values in column (ie histogram)"""
                 if column_distinct_count <= self.histogram_cutoff:
-
                     column_histogram_sql = 'SELECT COUNT(*) AS "column_value_count", {ansi_full_column_name} AS "column_value" FROM {ansi_full_table_name} GROUP BY {ansi_full_table_name}'.format(**column_info_dict)
                     try:
                         start_time = time.perf_counter()
@@ -205,8 +216,8 @@ class AutoTestOdbc(object):
                         column_histogram_pair_dict['column_value_count'] = column_histogram_pair[0]
                         column_histogram_pair_dict['column_value_string'] = column_histogram_pair[1]
                         column_histogram_pairs.append(column_histogram_pair_dict)
+                    """save to logging database with sqlalchemy"""
                     column_profile_id = self.profile_saver.log_column_histogram(column_histogram_pairs)
-                break
 class DenodoProfiler(AutoTestOdbc):
     def __init__(self, server_name, database_name):
         connection_lambda = lambda server_name, database_name, port=9999: "DSN={}".format(dw_denodo_dsn)
@@ -219,6 +230,7 @@ class DenodoProfiler(AutoTestOdbc):
 class SqlServerProfiler(AutoTestOdbc):
     def __init__(self, server_name, database_name):
         connection_lambda = lambda server_name, database_name: "DRIVER={ODBC Driver 11 for SQL Server};" + "SERVER={};DATABASE={};Trusted_Connection=Yes;".format(server_name, database_name)
+        connection_lambda = lambda server_name, database_name: "DRIVER={ODBC Driver 13 for SQL Server};" + "SERVER={};DATABASE={};Trusted_Connection=Yes;".format(server_name, database_name)
         # connection_lambda = lambda server_name, database_name, port=9999: "DSN={}".format(dw_sql_server_dsn)
 
         server_type = 'Sql Server'
--- pyGenericProfiler/AutoTestProfiler.py
+++ pyGenericProfiler/AutoTestProfiler.py
@@ -7,10 +7,12 @@ def main():
     start = time.perf_counter()
     deploy_sql_alchemy_model_database()
     server_name = 'SPDBDECSUP04'
+    server_name = 'PC'
     database_name = 'CommunityMart'
+    database_name = 'WideWorldImportersDW'
     mssql_profiler = SqlServerProfiler(server_name, database_name)
-    mssql_profiler.process_meta_data()
-    mssql_profiler.profile_database()
+    # mssql_profiler.process_meta_data()
+    # mssql_profiler.profile_database()
 
     print('total runtime: {}s'.format(time.perf_counter()-start))
 if __name__ == '__main__':
